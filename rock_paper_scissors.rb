be8062152c7deadb0fa2af7fe514c918a2ad1f26
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-07-18 18:00:53 -0400

    Add comments

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -2,6 +2,8 @@
 # By Gerrick Hilliard and Kevin Rocker
 # For TTS Code Immersion, Part-Time, Summer 2016
 
+# Using player_choice and computer_choice, return
+# whether or not the player wins as true/false.
 def player_wins?(player_choice, comp_choice)
   bool = (player_choice == "rock" && comp_choice == "scissors") ||
          (player_choice == "scissors" && comp_choice == "paper") ||
@@ -9,6 +11,7 @@ def player_wins?(player_choice, comp_choice)
   return bool
 end
 
+# Play a game of Rock-Paper-Scissors up to a specific score.
 def play_rps(max_score)
   options = ["rock", "paper", "scissors"]
   score = 0
